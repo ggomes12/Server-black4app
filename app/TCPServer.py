e5dce0ff4be5c26ed10cce499c7ce5569ecd9a10
Guilherme Gomes <guilherme.silva@ufpi.edu.br>
2024-03-28 21:37:33 -0300

    Update TCPServer.py

--- app/TCPServer.py
+++ app/TCPServer.py
@@ -28,52 +28,34 @@ def main():
             print(f"Conexão estabelecida por {addr}")
             while True:
                 data = conn.recv(1024)
-                #print("data no servidor", data)
-                opcao = data.decode().strip()
-                #print("Opção recebida:", opcao)
-                
+                if not data:
+                    break
+                opcao, *valores = data.decode().strip().split(',')
                 if opcao == '1':
-                    #print("Opção 1 selecionada.")
-                    num = int(conn.recv(1024).decode())
+                    num = int(valores[0])
                     print("Número recebido no servidor para fatorial:", num)
                     resultado = calcular_fatorial(num)
                     conn.sendall(str(resultado).encode())
-                    #print("Resultado enviado:", resultado)
-                    #print("Fatorial calculado.")
                 elif opcao == '2':
-                    #print("Opcao 2 selecionada.")
-                    n = int(conn.recv(1024).decode())
-                    k = int(conn.recv(1024).decode())
+                    n, k = map(int, valores)
                     print("Valores recebidos no servidor para combinação:", n, k)
                     resultado = calcular_combinacao(n, k)
                     conn.sendall(str(resultado).encode())
-                    #print("Resultado enviado:", resultado)
-                    #print("comb calculada")
                 elif opcao == '3':
-                    #print("Opcao 3 selecionada.")
-                    n = int(conn.recv(1024).decode())
-                    k = int(conn.recv(1024).decode())
+                    n, k = map(int, valores)
                     print("Valores recebidos no servidor para permutação:", n, k)
                     resultado = calcular_permutacao(n, k)
                     conn.sendall(str(resultado).encode())
-                    #print("Resultado enviado:", resultado)
-                    #print("permutação calculada")
                 elif opcao == '4':
-                    #print("Opcao 4 selecionada.")
-                    n = int(conn.recv(1024).decode())
-                    k = int(conn.recv(1024).decode())
+                    n, k = map(int, valores)
                     print("Valores recebidos no servidor para arranjo:", n, k)
                     resultado = calcular_arranjo(n, k)
                     conn.sendall(str(resultado).encode())
-                    #print("Resultado enviado:", resultado)
-                    #print("arranjo calculado")
                 elif opcao == '5':
                     print('Conexão encerrada.')
                     break
                 else:
                     conn.sendall("Opção inválida.".encode())
-                    #print("Opção inválida:", opcao)
 
 if __name__ == "__main__":
     main()
-    #teste
